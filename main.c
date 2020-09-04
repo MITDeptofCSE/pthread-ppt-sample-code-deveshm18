278401906d9a85b88a713dedf1749ab9ee618bb0
Devesh Mahajan <43674780+deveshm18@users.noreply.github.com>
2020-09-04 16:11:32 +0530

    Updated main.c
    Completed the program

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
-int main()
+#include<pthread.h>
+
+int sum;
+void *runner(void *param);
+
+int main(int argc,char *argv[])
 {
-  printf("Hello");
+  pthread.t tid;
+  pthread.attr.t attr;
+  if(args !=2) {
+    fprintf(stderr,"usage: a.out <integer value>\n");
+    return -1;
+  }
+  if(atoi(argv[1]<0)) {
+    fprintf(stderr,"%d must be >=0\n",atoi(argv[1]));
+    return -1;
+  }
+  pthread.attr.init(&attr);
+  pthread.create(&tid,&attr,&attr,runner,argv[1]);
+  pthread.join(tid,NULL);
+  printf("sum= %d\n",sum);
   //Add your code below
+}
+void *runner(void *param) {
+  int i,upper=atoi(param);
+  sum=0;
+  for(i=1;i<=upper;++i)
+    sum+=i;
+  pthread.exit(0);
 }
