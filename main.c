87bfbaef51cfcb18f7dd4e8b4f7674843c968ba1
Devesh Mahajan <43674780+deveshm18@users.noreply.github.com>
2020-09-04 16:26:34 +0530

    Update main.c
    Fixed typos

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ void *runner(void *param);
 
 int main(int argc,char *argv[])
 {
-  pthread.t tid;
-  pthread.attr.t attr;
+  pthread_t tid;
+  pthread_attr_t attr;
   if(args !=2) {
     fprintf(stderr,"usage: a.out <integer value>\n");
     return -1;
@@ -16,9 +16,9 @@ int main(int argc,char *argv[])
     fprintf(stderr,"%d must be >=0\n",atoi(argv[1]));
     return -1;
   }
-  pthread.attr.init(&attr);
-  pthread.create(&tid,&attr,&attr,runner,argv[1]);
-  pthread.join(tid,NULL);
+  pthread_attr_init(&attr);
+  pthread_create(&tid,&attr,&attr,runner,argv[1]);
+  pthread_join(tid,NULL);
   printf("sum= %d\n",sum);
   //Add your code below
 }
@@ -27,5 +27,5 @@ void *runner(void *param) {
   sum=0;
   for(i=1;i<=upper;++i)
     sum+=i;
-  pthread.exit(0);
+  pthread_exit(0);
 }
