ff1d1357d8d24e0f9e0628c8d2cc99148e217469
Devesh Mahajan <43674780+deveshm18@users.noreply.github.com>
2020-09-04 16:33:24 +0530

    Update main.c
    Corrected code with additional header file and type casting on necessary places

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<stdlib.h>
 
 int sum;
 void *runner(void *param);
@@ -8,22 +9,22 @@ int main(int argc,char *argv[])
 {
   pthread_t tid;
   pthread_attr_t attr;
-  if(args !=2) {
+  if(argc !=2) {
     fprintf(stderr,"usage: a.out <integer value>\n");
     return -1;
   }
-  if(atoi(argv[1]<0)) {
+  if(atoi(argv[1])<0) {
     fprintf(stderr,"%d must be >=0\n",atoi(argv[1]));
     return -1;
   }
   pthread_attr_init(&attr);
-  pthread_create(&tid,&attr,&attr,runner,argv[1]);
+  pthread_create(&tid,&attr,runner,argv[1]);
   pthread_join(tid,NULL);
   printf("sum= %d\n",sum);
   //Add your code below
 }
 void *runner(void *param) {
-  int i,upper=atoi(param);
+  int i,upper=atoi((char *)param);
   sum=0;
   for(i=1;i<=upper;++i)
     sum+=i;
